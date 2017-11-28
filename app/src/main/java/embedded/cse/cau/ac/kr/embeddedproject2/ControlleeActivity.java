e9ddc309baa04ef634c4c674ddc678a79733c0f5
freedomchurl <freedomchurl@naver.com>
2017-11-29 08:32:55 +0900

    Init Commit and Push

---
+++ app/src/main/java/embedded/cse/cau/ac/kr/embeddedproject2/ControlleeActivity.java
@@ -0,0 +1,19 @@
+package embedded.cse.cau.ac.kr.embeddedproject2;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by caucse on 2017-11-29.
+ */
+
+public class ControlleeActivity extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.controllee_activity);
+    }
+}
